0fbb0478238dc1dd9d6fcf0c15cbf9ff3784e577
Rahul-Tejwani <rahultejwani251213@gmail.com>
2024-11-11 18:53:02 +0530

    Updated Socket URL

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
   useEffect(() => {
     if (authUser) {
       const socket = io(
-        "https://whizzchat-connecting-your-loved-ones.onrender.com",
+        "https://rahul-tejwani-realtime-chat-app.onrender.com",
         {
           query: {
             userId: authUser._id,
